bf7ba46c2c4b9d377de1dbaf92e72dad2c5f7e3d
Bun Mengnguon <mengnguon@icloud.com>
2019-03-11 14:53:13 +0700

    create Article and learn to update, delete and find

---
+++ db/migrate/20190311051103_create_article.rb
@@ -0,0 +1,8 @@
+class CreateArticle < ActiveRecord::Migration[5.2]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.timestamp
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_03_11_052647) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "create_at"
+    t.datetime "update_at"
+  end
 
 end
