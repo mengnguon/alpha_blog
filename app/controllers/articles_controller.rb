af20ae12baf999ca7a32d268a0a24004e72b0d73
Bun Mengnguon <mengnguon@icloud.com>
2019-03-13 13:05:47 +0700

    Extract Destroy, Listing, Link

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,17 +1,24 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
+  def index
+    @articles = Article.all
+  end
+
+  def destroy
+    @article.destroy
+    flash[:notice] = "Article was successfully deleted"
+    redirect_to articles_path
+  end
 
   def new
-
     @article = Article.new
-
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
-
     @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was successfully created"
@@ -24,7 +31,6 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
       redirect_to article_path(@article)
@@ -34,10 +40,12 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   private
+    def set_article
+      @article = Article.find(params[:id])
+    end
 
     def article_params
       params.require(:article).permit(:title, :description)
