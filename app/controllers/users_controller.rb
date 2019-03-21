0898a237e3bb48b0ad1239b9fbb3ed48ed3ff436
Bun Mengnguon <mengnguon@icloud.com>
2019-03-21 14:12:01 +0700

    Edit user,show user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,28 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the alpha blog #{@user.username}"
+      redirect_to articles_path
     else
       render 'new'
     end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "You account was successfully updated"
+    redirect_to articles_path
+    else
+      reneder 'edit'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
 private
@@ -17,5 +36,7 @@ private
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 
   get 'signup', to: 'users#new'
 
+
   resources :users, except:[:new]
 
 end
